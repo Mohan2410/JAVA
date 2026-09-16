fb9054f63a795eed9d5f19698cac69cfc110ef6e
Mohan Gawande <mohangawande93@gmail.com>
2026-09-16 10:20:54 +0530

    each element is multiply by 10 and store in new array

---
+++ src/com/ArrayProblem/MultiplyBy10.java
@@ -0,0 +1,24 @@
+package com.ArrayProblem;
+
+public class MultiplyBy10 {
+    static int[] multiplyBy10(int[] arr){
+        int size = arr.length;
+        int newArray[] = new int[size];
+
+        for(int i=0;i<size;i++){
+            int element = arr[i];
+            int newElement = element * 10;
+            newArray[i] = newElement;
+        }
+        return newArray;
+    }
+    public static void main(String[] args) {
+        int[] arr = {2,3,4,5};
+        int ans[] = multiplyBy10(arr);
+        System.out.println("Printing ans array: ");
+        for(int i:ans){
+            System.out.println(i);
+        }
+
+    }
+}
